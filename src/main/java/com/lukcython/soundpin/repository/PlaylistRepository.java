2165a41597209a29e9a4871f313f8eb789c74b55
X1n9fU <minkyeong0244@gmail.com>
2024-11-02 01:28:58 +0900

    [feat] Query 수정

--- src/main/java/com/lukcython/soundpin/repository/PlaylistRepository.java
+++ src/main/java/com/lukcython/soundpin/repository/PlaylistRepository.java
@@ -2,17 +2,13 @@ package com.lukcython.soundpin.repository;
 
 import com.lukcython.soundpin.domain.Playlists;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface PlaylistRepository extends JpaRepository<Playlists, Long> {
 
     Optional<Playlists> findByPlaylistId(String playlistId);
-    Optional<Playlists> findByPin(String pin);
-
     @Query("SELECT p from Playlists p where p.pin=:pin")
     Optional<Playlists> findByPin(String pin);
 }
